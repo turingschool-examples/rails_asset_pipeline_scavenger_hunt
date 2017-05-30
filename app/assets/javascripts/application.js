b19db429b78365f38aa3a1aea81768953e90bf63
laurenfazah <laurenfazah@gmail.com>
2017-05-30 14:38:46 -0600

    Moved js from app.js into separate file

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,31 +14,3 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
-
-// UNCOMMENT LINES BELOW TO MAKE POKEMON RUN
-
-// $(document).ready(function(){
-//   animatePokemon();
-// });
-
-// function NewPosition(){
-//   var h = $(window).height();
-//   var w = $(window).width();
-//   console.log(Math.random())
-//   var nh = Math.random() * h;
-//   var nw = Math.random() * w;
-//   return [nh,nw];    
-// }
-
-// function animatePokemon(){
-//   $('.pokemon-container').each(function( index ) {
-//     var newPos = NewPosition();
-//     var duration = Math.floor(Math.random() * 10000);
-//     $(this).animate({ 
-//       top: newPos[0], 
-//       left: newPos[1] 
-//       }, duration, function(){
-//         animatePokemon();        
-//     });
-//   });
-// };
